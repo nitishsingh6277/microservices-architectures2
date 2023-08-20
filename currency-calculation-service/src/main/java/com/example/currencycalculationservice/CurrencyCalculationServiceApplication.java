36ec8c0c36a26c275866f28a80c17e6cadf2d485
nitishsingh6277 <nitishsingh6277@gmail.com>
2023-08-20 21:15:13 +0530

    add thfe fix

--- currency-calculation-service/src/main/java/com/example/currencycalculationservice/CurrencyCalculationServiceApplication.java
+++ currency-calculation-service/src/main/java/com/example/currencycalculationservice/CurrencyCalculationServiceApplication.java
@@ -1,13 +1,13 @@
-package com.tecnotab.currencyexchange;
+package com.example.currencycalculationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CurrencyExchangeServiceApplication {
+public class CurrencyCalculationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+		SpringApplication.run(CurrencyCalculationServiceApplication.class, args);
 	}
 
 }
