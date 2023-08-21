b3dfd37cc9effdc18d001300a62c543fec138d6e
nitishsingh6277 <nitishsingh6277@gmail.com>
2023-08-21 07:23:25 +0530

    add

--- currency-exchange-service/src/main/java/com/tecnotab/currencyexchange/controller/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/tecnotab/currencyexchange/controller/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.tecnotab.currencyexchange.controller;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,18 +7,22 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tecnotab.currencyexchange.model.ExchangeValue;
+import com.tecnotab.currencyexchange.repository.ExchangeValueRepository;
 
 @RestController
 public class CurrencyExchangeController {
 
 	@Autowired
 	private Environment environment;
+    
+	@Autowired
+	private ExchangeValueRepository repository;
 
-	
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		
-		ExchangeValue exchangeValue = new ExchangeValue(100L, "USD", "INR", BigDecimal.valueOf(70));
+		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
+		System.out.println(exchangeValue.getId());
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
 		return exchangeValue;
 	}
