f7fab531e6b43480f254e88d00e6bcda5a3d7b50
nitishsingh6277 <nitishsingh6277@gmail.com>
2023-08-21 11:00:59 +0530

    add

--- currency-calculation-service/src/main/java/com/technotab/currencycalculationservice/facade/CurrencyExchangeProxy.java
+++ currency-calculation-service/src/main/java/com/technotab/currencycalculationservice/facade/CurrencyExchangeProxy.java
@@ -1,5 +1,6 @@
 package com.technotab.currencycalculationservice.facade;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,7 +8,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.technotab.currencycalculationservice.model.CalculatedAmount;
 
 
-@FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+// @FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
+@FeignClient(name = "currency-exchange-service")
+@RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
         @GetMapping("/currency-exchange/from/{from}/to/{to}")
