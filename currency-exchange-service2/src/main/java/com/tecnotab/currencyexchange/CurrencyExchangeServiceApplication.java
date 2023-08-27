9f0b1737608bc8ebc23f2b5aae7fb3187e376dc0
nitishsingh6277 <nitishsingh6277@gmail.com>
2023-08-27 13:59:36 +0530

    aadd

---
+++ currency-exchange-service2/src/main/java/com/tecnotab/currencyexchange/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,17 @@
+package com.tecnotab.currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+}
