e3e8748ff15ae71e7b82bb597b4314ad1ed951cd
nitishsingh6277 <nitishsingh6277@gmail.com>
2023-10-13 23:57:52 +0530

    add

--- currency-calculation-service/src/main/java/com/technotab/currencycalculationservice/facade/CurrencyExchangeProxy.java
+++ currency-calculation-service/src/main/java/com/technotab/currencycalculationservice/facade/CurrencyExchangeProxy.java
@@ -9,11 +9,13 @@ import com.technotab.currencycalculationservice.model.CalculatedAmount;
 
 
 // @FeignClient(name = "currency-exchange-service", url = "http://localhost:8000")
-@FeignClient(name = "currency-exchange-service")
+//@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "netflix-zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 
-        @GetMapping("/currency-exchange/from/{from}/to/{to}")
+        //@GetMapping("/currency-exchange/from/{from}/to/{to}")
+        @GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
     	public CalculatedAmount retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 
     
